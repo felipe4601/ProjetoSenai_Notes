dc32967bbb00e66d887b2bc7d71c2267bdb77a7d
BrunoAlmeidaRamos <robrumi01@gmail.com>
2025-09-25 20:30:55 -0300

    criando model tag

--- backEnd/Senai-Notes/src/main/java/br/com/senai_notes/Senai/Notes/model/Tag.java
+++ backEnd/Senai-Notes/src/main/java/br/com/senai_notes/Senai/Notes/model/Tag.java
@@ -1,4 +1,32 @@
 package br.com.senai_notes.Senai.Notes.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tag",schema = "senai_notes")
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tag")
+    private Integer idTag;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="id_usuario",nullable = false)
+
+    private Usuario  usuario;
+
 }
