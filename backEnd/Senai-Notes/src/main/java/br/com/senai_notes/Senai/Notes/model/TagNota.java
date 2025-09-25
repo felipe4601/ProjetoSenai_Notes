58f11e792394789b89c86d522b681c4562b37073
Raphaelz3 <raphael.g.nascimento@aluno.senai.br>
2025-09-25 20:29:39 -0300

    Criação da TagNota

--- backEnd/Senai-Notes/src/main/java/br/com/senai_notes/Senai/Notes/model/TagNota.java
+++ backEnd/Senai-Notes/src/main/java/br/com/senai_notes/Senai/Notes/model/TagNota.java
@@ -1,4 +1,30 @@
 package br.com.senai_notes.Senai.Notes.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
 public class TagNota {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tag_nota")
+    private Long idTagNota;
+
+    @ManyToOne
+    @JoinColumn(name = "id_nota")
+    @Column(name = "id_nota",  nullable = false)
+    private Nota idNota;
+
+    @ManyToOne
+    @JoinColumn(name = "id_tag")
+    @Column(name = "id_tag", nullable = false)
+    private Tag idTag;
 }
