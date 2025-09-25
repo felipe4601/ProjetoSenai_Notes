53b3f40225b2d803c99f81485fae72e5817cd984
Marcel Gomes Cerri <marcel060883@gmail.com>
2025-09-24 21:35:44 -0300

    Criando api

---
+++ backEnd/Senai-Notes/src/main/java/br/com/senai_notes/Senai/Notes/SenaiNotesApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai_notes.Senai.Notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaiNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaiNotesApplication.class, args);
+	}
+
+}
---
+++ backEnd/Senai-Notes/src/test/java/br/com/senai_notes/Senai/Notes/SenaiNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai_notes.Senai.Notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenaiNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
