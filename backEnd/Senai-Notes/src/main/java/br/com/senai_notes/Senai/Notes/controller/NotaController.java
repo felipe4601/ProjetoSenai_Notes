507f8fc9270900dd0852e4ecaae5cd50eb7fbd67
felipeevaristo <felipeevaristo344@gmail.com>
2025-09-28 19:23:31 -0300

    :sparkles: feat CRUD finalizado para notas
    # Conflicts:
    #	backEnd/Senai-Notes/src/main/java/br/com/senai_notes/Senai/Notes/controller/NotaController.java
    #	backEnd/Senai-Notes/src/main/java/br/com/senai_notes/Senai/Notes/repository/NotaRepository.java
    #	backEnd/Senai-Notes/src/main/java/br/com/senai_notes/Senai/Notes/service/NotaService.java

---
+++ backEnd/Senai-Notes/src/main/java/br/com/senai_notes/Senai/Notes/controller/NotaController.java
@@ -0,0 +1,54 @@
+package br.com.senai_notes.Senai.Notes.controller;
+
+
+import br.com.senai_notes.Senai.Notes.model.Nota;
+import br.com.senai_notes.Senai.Notes.service.NotaService;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notas")
+public class NotaController {
+    private final NotaService notaService;
+
+    public NotaController(NotaService notaService) {
+        this.notaService = notaService;
+    }
+
+    // CRUD
+    // Método para cadastrar nota
+    @PostMapping
+    public ResponseEntity<?> cadastrarNota(@RequestBody Nota novaNota){
+        notaService.cadastrarNota(novaNota);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaNota);
+    }
+    // READ
+    // Método para mostrar nota
+    @GetMapping("/{email}")
+    public  ResponseEntity<?> listarNotasUsuario(@PathVariable String email){
+        List<Nota> notas = notaService.listarPorUsuario(email);
+        return ResponseEntity.ok(notas);
+    }
+
+    // UPDATE
+    // Método para atualizar nota
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editarNota(@PathVariable Integer id, @RequestBody Nota notaAtualizada){
+        Nota notaExistente = notaService.atualizarNota(id, notaAtualizada);
+        return ResponseEntity.ok(notaExistente);
+    }
+
+    // DELETE
+    // Método para deletar nota
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> removerNota(@PathVariable Integer id){
+        notaService.removerNota(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
--- backEnd/Senai-Notes/src/main/java/br/com/senai_notes/Senai/Notes/repository/NotaRepository.java
+++ backEnd/Senai-Notes/src/main/java/br/com/senai_notes/Senai/Notes/repository/NotaRepository.java
@@ -1,9 +1,15 @@
 package br.com.senai_notes.Senai.Notes.repository;
 
 import br.com.senai_notes.Senai.Notes.model.Nota;
+import br.com.senai_notes.Senai.Notes.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface NotaRepository extends JpaRepository<Nota, Integer> {
+    List<Nota> findByUsuarioEmail(String email);
+
 }
