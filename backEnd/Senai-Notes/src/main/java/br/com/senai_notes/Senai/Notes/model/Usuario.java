2b459e5709c37467ab26e2dbb0ab35d674f9d44d
Raphaelz3 <raphael.g.nascimento@aluno.senai.br>
2025-10-03 21:35:23 -0300

    :sparkles: feat Impleentação de segurança

--- backEnd/Senai-Notes/src/main/java/br/com/senai_notes/Senai/Notes/model/Usuario.java
+++ backEnd/Senai-Notes/src/main/java/br/com/senai_notes/Senai/Notes/model/Usuario.java
@@ -1,6 +1,7 @@
 package br.com.senai_notes.Senai.Notes.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -9,6 +10,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,7 +25,7 @@ import lombok.Setter;
 @Entity
 
 @Table(name = "usuario" )
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,4 +45,50 @@ public class Usuario {
 
     @Column(name = "nome", nullable = true, columnDefinition = "TEXT")
     private String nome;
+
+    @Override
+    @JsonIgnore
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        return List.of();
+    }
+
+    @Override
+    @JsonIgnore
+    public String getPassword() {
+
+        return this.senha;
+    }
+
+    @Override
+    @JsonIgnore
+    public String getUsername() {
+
+        return this.email;
+    }
+
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isEnabled() {
+        return true;
+    }
 }
