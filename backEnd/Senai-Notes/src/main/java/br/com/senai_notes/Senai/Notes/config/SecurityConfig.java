8bd1bc6c799df78a0bffacf08ac81c0972d61929
Raphael <raphel-nascimento@hotmail.com>
2025-10-04 17:31:56 -0300

    :arrow_up: raw atualização da segurança e login

--- backEnd/Senai-Notes/src/main/java/br/com/senai_notes/Senai/Notes/config/SecurityConfig.java
+++ backEnd/Senai-Notes/src/main/java/br/com/senai_notes/Senai/Notes/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers(HttpMethod.POST ,"/api/usuario").permitAll()
-                        .anyRequest().authenticated()//permitAll() todo vericar autorização
+                        .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
